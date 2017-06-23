ffd39313e2e2978bc971820105dbb19568999f13
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-06-23 17:15:05 +0100

    add redux router reducer to store

---
+++ client/reducer/count.js
@@ -0,0 +1,21 @@
+import { Map } from 'immutable';
+import { ADD } from '../action/count';
+
+const initialState = Map({
+  current: 0,
+});
+
+function addCount(state) {
+  return Map({
+    current: state.get('current') + 1,
+  });
+}
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD:
+      return addCount(state);
+    default:
+      return state;
+  }
+}
