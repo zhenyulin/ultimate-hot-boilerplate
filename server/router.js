895e67d12b9a3b9847cc98713c77692e82b08de3
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-06-23 14:34:34 +0100

    add server side route

--- server/router.js
+++ server/router.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import index from 'server/handler/index';
+import index from './handler/index';
+import info from './handler/info';
 
 const router = express.Router();
 
+router.use('/info', info);
 router.use(index);
 
 export default router;
