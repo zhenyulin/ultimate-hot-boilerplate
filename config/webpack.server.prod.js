8fc95f262abc0dbf0f673233f08ffca81dc691b8
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-06-23 15:46:02 +0100

    add webpack.server.prod config

---
+++ config/webpack.server.prod.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './server/index',
+  output: {
+    path: path.resolve('./dist/server'),
+    filename: 'server.js',
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      use: 'babel-loader',
+      exclude: /node_modules/,
+    }],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        BUILD_TARGET: JSON.stringify('server'),
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      minimize: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+  ],
+};
