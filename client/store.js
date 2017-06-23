b8c5e62fa84b7ffc54482d88bad42d4dc7c5fbfb
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-06-23 16:34:23 +0100

    add redux-dev-extension and redux-segment to the store

--- client/store.js
+++ client/store.js
@@ -1,13 +1,26 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
+import { createTracker } from 'redux-segment';
 
 import reducer from './reducer';
 
 export default function setupStore(history) {
   const middleware = [
     routerMiddleware(history),
+    createTracker(),
   ];
-  const store = applyMiddleware(...middleware)(createStore)(reducer);
+
+  let store;
+
+  if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
+    /* eslint-disable no-underscore-dangle */
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    /* eslint-enable */
+    const enhancer = composeEnhancers(applyMiddleware(...middleware));
+    store = createStore(reducer, enhancer);
+  } else {
+    store = applyMiddleware(...middleware)(createStore)(reducer);
+  }
 
   if (module.hot) {
     module.hot.accept('./reducer', () => {
