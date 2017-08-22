096a890631c655a8a50e86cbbdc026f2d7db6dbe
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-08-22 16:21:32 +0100

    git subrepo clone https://github.com/zhenyulin/redux-action-manager.git client/utils/action-manager
    subrepo:
      subdir:   "client/utils/action-manager"
      merged:   "fc4a3ba"
    upstream:
      origin:   "https://github.com/zhenyulin/redux-action-manager.git"
      branch:   "master"
      commit:   "fc4a3ba"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ client/utils/action-manager/src/constant.js
@@ -0,0 +1,9 @@
+// @flow
+export const ASYNC = {
+  IDLE: 'IDLE',
+  PROCESS: 'PROCESS',
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR',
+};
+
+export default ASYNC;
---
+++ client/utils/action-manager/src/reducer.js
@@ -0,0 +1,49 @@
+// @flow
+
+import { Map } from 'immutable';
+import { ASYNC } from './constant';
+import type { Action, AsyncActionNames } from './types';
+
+const initialState = Map({
+  status: ASYNC.IDLE,
+  isFetching: false,
+  isError: false,
+  data: {},
+  error: {},
+});
+
+export const immutableAsyncReducers = (
+  ASYNC_ACTION_NAMES: AsyncActionNames,
+) => (state: Map = initialState, action: Action) => {
+  switch (action.type) {
+    case ASYNC_ACTION_NAMES.GET:
+    case ASYNC_ACTION_NAMES.POST:
+      return state.merge({
+        status: ASYNC.PROCESS,
+        isFetching: true,
+        isError: false,
+      });
+    case ASYNC_ACTION_NAMES.RECEIVE:
+      return state.merge({
+        status: ASYNC.SUCCESS,
+        isFetching: false,
+        isError: false,
+        data: action.payload,
+      });
+    case ASYNC_ACTION_NAMES.ERROR:
+      return state.merge({
+        status: ASYNC.ERROR,
+        isFetching: false,
+        isError: true,
+        error: action.payload,
+      });
+    case ASYNC_ACTION_NAMES.RESET:
+      return state.merge(initialState);
+    default:
+      return state;
+  }
+};
+
+// *TODO: adding normal javascript state store support
+
+export default immutableAsyncReducers;
