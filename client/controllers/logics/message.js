076fa8896090f9db130dd51009f624fffb8a559e
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-10-30 19:20:43 +0000

    minimise count reducer

--- client/controllers/logics/message.js
+++ client/controllers/logics/message.js
@@ -6,7 +6,7 @@ import { MESSAGE, messageActions } from 'controllers/actions/message';
 const getMessageLogic = createLogic({
   type: MESSAGE.GET,
   process({ getState }, dispatch, done) {
-    const index = getState().getIn(['count', 'current']);
+    const index = getState().getIn(['count']);
     fetch(`/message/${index}`)
       .then(response => response.json())
       .then(data => dispatch(messageActions.receive(data)))
