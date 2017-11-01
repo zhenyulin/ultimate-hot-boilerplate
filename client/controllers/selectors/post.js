c8fffd74a63263cb9ba3fb045dea3062edfbe9b7
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-11-01 11:03:57 +0000

    enhance type safety for post data flow

--- client/controllers/selectors/post.js
+++ client/controllers/selectors/post.js
@@ -1,13 +1,26 @@
+// @flow
+
 import { createSelector } from 'reselect';
 
-export const getSelectedId = state => state.getIn(['post', 'selected']);
-export const getPosts = state =>
+import type { Map } from 'immutable';
+import type {
+  Post,
+  Author,
+  Comment,
+  PopulatedPost,
+} from 'controllers/types/post';
+
+export const getSelectedId = (state: Map<string, any>): string =>
+  state.getIn(['post', 'selected']);
+export const getPosts = (state: Map<string, any>): Map<string, Post> =>
   state.getIn(['post', 'normalized', 'entities', 'posts']);
-export const getComments = state =>
+export const getComments = (state: Map<string, any>): Map<string, Comment> =>
   state.getIn(['post', 'normalized', 'entities', 'posts']);
-export const getAuthors = state =>
+export const getAuthors = (state: Map<string, any>): Map<string, Author> =>
   state.getIn(['post', 'normalized', 'entities', 'authors']);
-export const getPostData = state => state.getIn(['post', 'data']);
+export const getPostData = (
+  state: Map<string, any>,
+): Map<string, PopulatedPost> => state.getIn(['post', 'data']);
 
 export const getSelectedPost = createSelector(
   [getPostData, getSelectedId],
