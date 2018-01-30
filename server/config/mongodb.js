69c403b0aab6c8a7eeb8b0d6a46b38d6d26a5dcd
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-01-30 23:51:49 +0000

    mongoose update

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,14 +1,10 @@
 import mongoose from 'mongoose';
 import { MONGO_DB_URI } from 'server/constant';
 
-const DB_OPTIONS = {
-  useMongoClient: true,
-};
-
 export default function connectMongoDB() {
   console.time('mongodb connected');
   mongoose.Promise = global.Promise;
-  mongoose.connect(MONGO_DB_URI, DB_OPTIONS);
+  mongoose.connect(MONGO_DB_URI);
 
   /* istanbul ignore next */
   if (process.env.NODE_ENV === 'development') {
